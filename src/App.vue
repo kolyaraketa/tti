4e53acdff9f98e13f8eb525d843cd9a121d081a8
Kolya Raketa <raketakolya@gmail.com>
2020-10-01 17:08:04 +0300

    complited allToAllTournament calculation

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,90 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import playersData from '@/data/players.json';
+import tournamentsData from '@/data/tournaments.json';
 
-#nav {
-  padding: 30px;
+const TOURNAMENT_TYPES = {
+  1: 'allToAllTournament',
+  2: 'groupTournament'
+};
+const POINTS_FOR_VICTORY = 3;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {};
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
+  created() {
+    this.init();
+  },
+
+  methods: {
+    init() {
+      this.prepareData();
+    },
+
+    prepareData() {
+      tournamentsData.data.forEach((tournament) => {
+        this[TOURNAMENT_TYPES[tournament.type]](tournament);
+      });
+    },
+
+    allToAllTournament(tournament) {
+      const playersSumm = tournament.participants.reduce((summ, playerId) => {
+        const score = tournament.games.reduce((scoreRes, game) => {
+          if (!game.players.includes(playerId)) return scoreRes;
+          const playerIndex = game.players.indexOf(playerId);
+          const opponentIndex = playerIndex === 0 ? 1 : 0;
+          if (game.score[playerIndex] > game.score[opponentIndex]) scoreRes += POINTS_FOR_VICTORY;
+          return scoreRes;
+        }, 0);
+
+        summ.push({ playerId, score });
+        return summ;
+      }, []);
+      playersSumm
+        .sort((a, b) => a.score - b.score)
+        .forEach(({ playerId, score }) => {
+          playersData[playerId].rating += +score / POINTS_FOR_VICTORY;
+          playersData[playerId].history.push({ date: tournament.date, score: +score / 3 });
+        });
+      console.log(playersSumm);
+    },
+
+    groupTournament(tournament) {
+      console.log('groupTournament -> tournament', tournament);
+      console.log(tournamentsData);
+      console.log(playersData);
     }
   }
-}
+};
+</script>
+
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+
+#nav
+  padding: 30px
+
+  a
+    font-weight: bold
+    color: #2c3e50
+
+    &.router-link-exact-active
+      color: #42b983
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import TournamentListElement from '@/components/TournamentListElement.vue';
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    // TournamentListElement
+  },
+  data() {
+    return {};
+  },
+
+  created() {
+    this.init();
+  },
+
+  methods: {
+    init() {}
   }
 };
 </script>
